eabb1cd1c60704dd08daa54d09b3bcdd3eb7057f
Johan Groenen <johan.groenen+github@gmail.com>
2020-03-21 16:02:59 +0100

    pdo

--- private/scripts/import_from_api.php
+++ private/scripts/import_from_api.php
@@ -3,6 +3,19 @@
 $json = file_get_contents('https://cbase.codefor.nl/cbases');
 $data = json_decode($json, true);
 
+$conf = [
+  'db' => [
+    'name' => 'cbase',
+    'host' => 'localhost',
+    'port' => '3306',
+    'user' => 'cbase',
+    'pass' => 'password' // ENV
+  ]
+];
+
+$dsn = "mysql:dbname={$conf['db']['name']};host={$conf['db']['host']};port={$conf['db']['port']};charset=utf8";
+$pdo = new PDO($dsn, $conf['db']['user'], $conf['db']['pass']);
+
 $fields = [
   'cbase' => [
     'id', // FIXME: create some unique universal identifier too
@@ -44,18 +57,25 @@ $fields = [
 foreach ($data['_embedded']['cbase'] as $cbase) {
     // sql insert usecase
     $sql = "INSERT INTO cbase SET\n";
+    $values = [];
     foreach ($fields['cbase'] as $field) {
       $sql .= "{$field} = :{$field} /* {$cbase[$field]} */\n";
+      $values[$field] = $cbase[$field];
     }
     var_dump($sql);
-    // pdo
+    $stmt = $pdo->prepare($sql);
+    //$stmt->execute($values);
     foreach ($cbase['_embedded']['usecase'] as $usecase) {
         // sql insert usecase
         $sql = "INSERT INTO usecase SET\n";
+        $values = [];
         foreach ($fields['usecase'] as $field) {
           $sql .= "{$field} = :{$field} /* {$usecase[$field]} */\n";
+          $values[$field] = $cbase[$field];
         }
         var_dump($sql);
+        $stmt = $pdo->prepare($sql);
+        //$stmt->execute($values);
         exit();
     }
 }
